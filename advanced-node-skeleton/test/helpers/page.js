ba76d0b1e1c8815c254f3c969531e61b63dec0bd
Drago96 <dragproychev@gmail.com>
2018-04-10 04:03:16 +0300

    Switched chromium to headless mode

--- advanced-node-skeleton/test/helpers/page.js
+++ advanced-node-skeleton/test/helpers/page.js
@@ -5,7 +5,7 @@ const userFactory = require("../factories/userFactory");
 class Page {
     static async build() {
         const browser = await puppeteer.launch({
-            headless: false,
+            headless: true,
             args: ["--no-sandbox"]
         });
 
